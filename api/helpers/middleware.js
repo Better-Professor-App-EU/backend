c87c8b74df5e24a33b60d78471c8d29656071bc1
CarnunMP <carnun@hotmail.co.uk>
2019-11-18 19:38:18 +0000

    logger middleware written; usersRouter added to server.js

---
+++ api/helpers/middleware.js
@@ -0,0 +1,6 @@
+module.exports = {
+  logger: function(req, res, next) {
+    console.log(req.method, req.url, Date());
+    next();
+  }
+}
--- api/server.js
+++ api/server.js
@@ -5,12 +5,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const server = express();
-const middleware = require('./helpers/middleware');
+const { logger } = require('./helpers/middleware');
 const usersRouter = require('./users/users-router');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/', logger);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("<h2>We're in business!<h2>");

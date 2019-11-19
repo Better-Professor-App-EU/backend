610ac0c7b03e6c017cc8a58564c378ea6e2b6a3f
CarnunMP <carnun@hotmail.co.uk>
2019-11-19 19:54:21 +0000

    added a little scaffolding to messages-router

---
+++ api/messages/messages-router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const Messages = require('./messages-model');
+const { genericError } = require('../helpers/helpers');
+
+const router = express.Router();
+
+module.exports = router;

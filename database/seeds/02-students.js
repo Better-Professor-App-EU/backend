176505d9aee8c2187555a3cdcfdcf19172600ce3
CarnunMP <carnun@hotmail.co.uk>
2019-11-19 00:29:28 +0000

    fixed indentation in seed files

--- database/seeds/02-students.js
+++ database/seeds/02-students.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
-      return knex('Students').insert([
-        {id: 1, name: 'Mickey The Drink'},
-        {id: 2, name: 'Steve Bytheway'},
-        {id: 3, name: 'Harry Harriman'},
-        {id: 4, name: 'Gary Cheeseman'},
-        {id: 5, name: 'Neil Overall'},
-        {id: 6, name: 'Gary Dungaree'},
-        {id: 7, name: 'John Caramel'},
-        {id: 8, name: 'Ron Waffle'}
-      ]);
+  return knex('Students').insert([
+    {id: 1, name: 'Mickey The Drink'},
+    {id: 2, name: 'Steve Bytheway'},
+    {id: 3, name: 'Harry Harriman'},
+    {id: 4, name: 'Gary Cheeseman'},
+    {id: 5, name: 'Neil Overall'},
+    {id: 6, name: 'Gary Dungaree'},
+    {id: 7, name: 'John Caramel'},
+    {id: 8, name: 'Ron Waffle'}
+  ]);
 };

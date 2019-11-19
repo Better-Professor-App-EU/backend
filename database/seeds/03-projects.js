14097b61df5ea7f341f1d0ac0565c95455027df4
CarnunMP <carnun@hotmail.co.uk>
2019-11-19 23:50:48 +0000

    fixed seed files for pg (?)

--- database/seeds/03-projects.js
+++ database/seeds/03-projects.js
@@ -1,11 +1,11 @@
 
 exports.seed = function(knex) {
   return knex('Projects').insert([
-    {id: 1, name: 'Letter of Recommendation'},
-    {id: 2, name: 'Feedback on Popper Essay'},
-    {id: 3, name: 'Project Feedback'},
-    {id: 4, name: 'Email'},
-    {id: 5, name: 'Give Stern Talking To'},
-    {id: 6, name: 'Notify of Department Prize Nomination'}
+    {name: 'Letter of Recommendation'},
+    {name: 'Feedback on Popper Essay'},
+    {name: 'Project Feedback'},
+    {name: 'Email'},
+    {name: 'Give Stern Talking To'},
+    {name: 'Notify of Department Prize Nomination'}
   ]);
 };
--- database/seeds/07-messages.js
+++ database/seeds/07-messages.js
@@ -1,16 +1,16 @@
 
 exports.seed = function(knex) {
   return knex('Messages').insert([
-    {id: 1, user_id: 1, student_id: 1, text: "TEST. TEST! DO YOU READ ME? TEST.", send_to_self: '1', timestamp: JSON.stringify(new Date())},
-    {id: 2, user_id: 1, student_id: 2, text: "Roger roger.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 3, user_id: 1, student_id: 3, text: "Do you copy? OVER.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 4, user_id: 1, student_id: 4, text: "Anyone there?", send_to_self: '1', timestamp: JSON.stringify(new Date())},
-    {id: 5, user_id: 2, student_id: 3, text: "'Sup?", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 6, user_id: 2, student_id: 4, text: "Sooo...", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 7, user_id: 2, student_id: 5, text: "Yooooooo!", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 8, user_id: 2, student_id: null, text: "Echooo echo echo...", send_to_self: '1', timestamp: JSON.stringify(new Date())},
-    {id: 9, user_id: 3, student_id: 7, text: "Hello? Please respond.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 10, user_id: 3, student_id: 8, text: "URGENT ACTION REQUIRED!!!", send_to_self: '0', timestamp: JSON.stringify(new Date())},
-    {id: 11, user_id: 3, student_id: 8, text: "Scratch that. All good!", send_to_self: '0', timestamp: JSON.stringify(new Date())}
+    {user_id: 1, student_id: 1, text: "TEST. TEST! DO YOU READ ME? TEST.", send_to_self: '1', timestamp: JSON.stringify(new Date())},
+    {user_id: 1, student_id: 2, text: "Roger roger.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 1, student_id: 3, text: "Do you copy? OVER.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 1, student_id: 4, text: "Anyone there?", send_to_self: '1', timestamp: JSON.stringify(new Date())},
+    {user_id: 2, student_id: 3, text: "'Sup?", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 2, student_id: 4, text: "Sooo...", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 2, student_id: 5, text: "Yooooooo!", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 2, student_id: null, text: "Echooo echo echo...", send_to_self: '1', timestamp: JSON.stringify(new Date())},
+    {user_id: 3, student_id: 7, text: "Hello? Please respond.", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 3, student_id: 8, text: "URGENT ACTION REQUIRED!!!", send_to_self: '0', timestamp: JSON.stringify(new Date())},
+    {user_id: 3, student_id: 8, text: "Scratch that. All good!", send_to_self: '0', timestamp: JSON.stringify(new Date())}
   ]);
 };
